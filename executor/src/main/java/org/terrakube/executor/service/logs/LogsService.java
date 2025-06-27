b285bac40b036a6e19197c71a649880793f23329
Prashant <P50040741@capita.co.uk>
2025-06-27 11:54:33 +0000

    feature/add-support-for-proxy changes merged to main

--- executor/src/main/java/org/terrakube/executor/service/logs/LogsService.java
+++ executor/src/main/java/org/terrakube/executor/service/logs/LogsService.java
@@ -2,31 +2,72 @@ package org.terrakube.executor.service.logs;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.terrakube.client.TerrakubeClient;
+import org.terrakube.client.model.organization.job.Log;
+import org.terrakube.client.model.organization.job.LogsRequest;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 @Service
 @Slf4j
 @AllArgsConstructor
+@ConditionalOnProperty(name = "org.executor.log-though-api", havingValue = "true", matchIfMissing = false)
 public class LogsService implements ProcessLogs {
+    private TerrakubeClient terrakubeClient;
+    private final LinkedBlockingDeque<Log> logQueue = new LinkedBlockingDeque<>();
 
-    RedisTemplate redisTemplate;
-
-    @Override
     public void sendLogs(Integer jobId, String stepId, int lineNumber, String output) {
-        Map<String, String> streamData = new LinkedHashMap();
-        streamData.put("jobId", String.valueOf(jobId));
-        streamData.put("stepId", stepId);
-        streamData.put("lineNumber", String.valueOf(lineNumber));
-        streamData.put("output", output);
+        Log logEntry = new Log();
+        logEntry.setJobId(jobId);
+        logEntry.setStepId(stepId);
+        logEntry.setLineNumber(lineNumber);
+        logEntry.setOutput(output);
 
-        redisTemplate.opsForStream().add(jobId.toString(), streamData);
+        try {
+            logQueue.put(logEntry);
+        } catch (InterruptedException addLogException) {
+            log.error("Failed to add log to queue", addLogException);
+        }
     }
 
-    public void deleteLogs(String jobId) {
-        redisTemplate.delete(jobId);
+    @Scheduled(fixedDelay = 5000)  // Send logs every 5 seconds
+    public void sendBatchedLogs() {
+        log.info("Sending logs to Terrakube API");
+        if (logQueue.isEmpty()) {
+            return;
+        }
+
+        List<Log> batch = new ArrayList<>();
+        logQueue.drainTo(batch);
+
+        if (batch.isEmpty()) {
+            return;
+        }
+
+        LogsRequest logsRequest = new LogsRequest();
+        logsRequest.setData(batch);
+
+        try {
+            terrakubeClient.appendLogs(logsRequest);
+        } catch (Exception sendLogsException) {
+            log.error("Failed to send logs", sendLogsException);
+            requeueLogs(batch);
+        }
+    }
+
+
+    private void requeueLogs(List<Log> failedBatch) {
+        for (int i = failedBatch.size() - 1; i >= 0; i--) {
+            try {
+                logQueue.putFirst(failedBatch.get(i));
+            } catch (InterruptedException e) {
+                log.error("Thread interrupted while re-queuing logs", e);
+            }
+        }
     }
 }
